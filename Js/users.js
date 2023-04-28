ccf22fa8e88927a7dfb01051bbea7b5cc52e59c2
jorge munarriz <munarrizjorge@gmail.com>
2023-04-28 11:25:01 +0200

    user JS and ids

--- Js/users.js
+++ Js/users.js
@@ -1,4 +1,18 @@
-let player = []
-let input = document.querySelector("#inputName")
+let player = { name: "Pedro" };
+let input = document.querySelector("#inputName");
+const page1 = document.querySelector("#mainPage")
+const page2 = document.querySelector("#mainPage2") 
+const page3 = document.querySelector("#mainPage3")
+const page4 = document.querySelector("#mainPage4")
+const page5 = document.querySelector("#mainPage5")
 
 localStorage.setItem("player", JSON.stringify(player));
+const playerLS = localStorage.getItem("player");
+const playerObj = JSON.parse(playerLS);
+console.log(playerObj)
+input.addEventListener("click", () => {
+  if (player.name.length > 3 && player.name.length < 15) {
+    user = localStorage.getItem("player");
+
+  }
+});
