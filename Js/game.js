0f8b58f1483fea400ec359cd09e0edfea855b50d
Matias Alaimo <matias.chiappa@hotmail.com>
2023-04-28 12:55:00 +0200

    moved function shuffle from main to game

---
+++ Js/game.js
@@ -0,0 +1,16 @@
+function shuffleEasy() {
+    const cards = ["Batman", "Captain America", "Catwoman", "Green lantern", "Groot", "Hulk", "Iron man", "Spiderman", "Wolverine", "card-9", "card-10"];
+    while (shuffledCards.length < 8) {
+        const randomIndex = Math.floor(Math.random() * cards.length);
+        const card = cards[randomIndex];
+        if (!shuffledCards.includes(card)) {
+        shuffledCards.push(card);
+            cards.splice(randomIndex, 1);
+        }
+        }
+    shuffledCards = shuffledCards.concat(shuffledCards);
+    for(let i = 0; i < shuffledCards.length; i++) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledCards[i], shuffledCards[j]] =[shuffledCards[j], shuffledCards[i]]
+}
+};
