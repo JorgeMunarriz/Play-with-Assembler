2a78e6f83b8fcf36de237b792d031009c94f39ef
Matias Alaimo <matias.chiappa@hotmail.com>
2023-04-28 13:08:04 +0200

    finished createscore function

--- Js/score.js
+++ Js/score.js
@@ -4,13 +4,24 @@ const scoreboard = document.querySelector("#scoreboard");
 const inputName = document.querySelector("#inputName");
 //Input buton selector
 const btnPlay = document.querySelector(".btn-play");
-
-//Function to store value of input on localStorage
-
+//Succeses Variable
+let playerSucces = 0;
+//Function to create scoreboard item
 function createScore(){
     const newPlayer = document.createElement("h3");
     newPlayer.textContent = localStorage.getItem('player');
-}
+    scoreboard.appendChild(newPlayer);
+
+    const timePassed = document.createElement("p");
+    timePassed.setAttribute("class", "time-passed");
+    timePassed.setAttribute("id", "timePassed");
+    scoreboard.appendChild(timePassed);
+
+    const succeses = document.createElement("p");
+    succeses.setAttribute("class", "player-score");
+    succeses.setAttribute("id", "playerScore");
+    scoreboard.appendChild(succeses);
+};
 
 //Eventlistener to btnPlay
 btnPlay.addEventListener("click", saveNameToLocal)
