f9fa586cb1cfef322d9656144f98966a6320762b
kevinPolania <kevin.narvaez.rc@gmail.com>
2023-04-28 13:17:24 +0100

    "ordenando archivos"

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+/* ---------Page 2   */ 
+// para que se cargue le documento
+document.addEventListener("DOMContentLoaded", function() {
+
+    // formulario y el botón de Play
+    const form = document.querySelector("#formUser");
+    const btnPlay = document.querySelector("#btnPlay");
+  
+    //  formulario
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); // Evita la recarga de la página
+      const inputName = document.querySelector("#inputName");
+  
+      // valor positivo
+      if (inputName.value.trim() !== "") {
+        // Ocultar la primera pagina y visualizar la segunda
+        const mainPage = document.querySelector("#mainPage");
+        const mainPage2 = document.querySelector("#mainPage2");
+        mainPage.hidden = true;
+        mainPage2.hidden = false;
+  
+        // texto pagina 2
+        const option = document.querySelector(".option");
+        option.textContent = `Choose An Option, ${inputName.value}!`;
+      }
+    });
+  
+  });
+
+
+
+/*----------page 2      */ 
 let shuffledCards = []
 const page1 = document.querySelector("#mainPage")
 const page2 = document.querySelector("#mainPage2") 
