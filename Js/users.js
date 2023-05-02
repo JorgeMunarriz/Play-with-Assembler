099c390651914b44654ff63bbbff07b92552bb99
jorge munarriz <munarrizjorge@gmail.com>
2023-05-02 11:15:01 +0200

    createCardEasy

--- Js/users.js
+++ Js/users.js
@@ -1,26 +1,32 @@
 //Name Input
 
-
 //Funcion Input
-function pickName(){
-    let newPlayer = inputName.value;
-    if (newPlayer.length > 3 && newPlayer.length < 15){
-        localStorage.setItem("player", newPlayer)
-    } else return
-};
+function pickName() {
+  let newPlayer = inputName.value;
+  if (newPlayer.length > 3 && newPlayer.length < 15) {
+    localStorage.setItem("player", newPlayer);
+  } else return;
+}
 //EventLIstener
-inputName.addEventListener("input", function(event){
-    event.preventDefault;
-    pickName()
-})
+inputName.addEventListener("input", function (event) {
+  event.preventDefault;
+  pickName();
+});
 
+function createCardsEasy() {
+  const gameDifficult = document.querySelector("#gameDifficult");
 
-function createCardsEasy (){
- let cardsDiv = document.createElement("div")
- let cardsContainer = document.createElement("div")
+  for (let i = 0; i < 16; i++) {
+    const cardsContainer = document.createElement("div");
+    const cardsImg = document.createElement("img");
+    const cardsBack = document.createElement("div");
 
- cardsContainer.classList.add("card-container")
- 
+    cardsContainer.classList.add("card-container");
+    cardsImg.classList.add("card");
+    cardsBack.classList.add("card-back");
 
-
+    cardsContainer.appendChild(gameDifficult);
+    cardsImg.appendChild(cardsContainer);
+    cardsBack.insertAdjacentElement("afterend", cardsImg);
+  }
 }
